7d9ccf27406c948dfa5e6197c40dd40b161bebf9
Juan Angosto Herrmann <juan.angosto.h@gmail.com>
2017-09-30 18:49:22 +0000

    Remove cpp files and add its lines in h file directly

--- Rele/Rele.h
+++ Rele/Rele.h
@@ -6,7 +6,6 @@
 class Rele
 {
     public:
-        Rele();
         Rele(int);
 
         int getPinNum();
@@ -18,5 +17,39 @@ class Rele
         int pinNum;
         boolean enabled;
 };
+
+Rele::Rele(int pin)
+{
+    pinNum = pin;
+    pinMode(pinNum, OUTPUT);
+    enabled = false;
+}
+
+int Rele::getPinNum()
+{
+    return pinNum;
+}
+
+boolean Rele::getEnabled()
+{
+    return enabled;
+}
+
+boolean Rele::enable()
+{
+    digitalWrite(pinNum, LOW);
+    enabled = true;
+
+    return enabled;
+}
+
+boolean Rele::disable()
+{
+    digitalWrite(pinNum, HIGH);
+    enabled = false;
+
+    return enabled;
+}
+
 
 #endif
--- TimedDevice/TimedDevice.h
+++ TimedDevice/TimedDevice.h
@@ -7,7 +7,6 @@
 class TimedDevice
 {
     public:
-        TimedDevice();
         TimedDevice(int, unsigned long, unsigned long);
 
         int getPinNum();
@@ -29,5 +28,78 @@ class TimedDevice
 	unsigned long increaseCurrentTime();
 	void resetCurrentTime();
 };
+
+TimedDevice::TimedDevice(int pin, unsigned long newOnTime, unsigned long newOffTime)
+{
+    pinNum = pin;
+    onTime = newOnTime;
+    offTime = newOffTime;
+    enabled = false;
+    currentTime = 0;
+
+    rele = new Rele(pin);
+}
+
+int TimedDevice::getPinNum()
+{
+    return pinNum;
+}
+
+unsigned long TimedDevice::getOffTime()
+{
+    return offTime;
+}
+
+unsigned long TimedDevice::getOnTime()
+{
+    return onTime;
+}
+
+boolean TimedDevice::getEnabled()
+{
+    return enabled;
+}
+
+unsigned long TimedDevice::setOffTime(unsigned long time)
+{
+    offTime = time;
+    return offTime;
+}
+
+unsigned long TimedDevice::setOnTime(unsigned long time)
+{
+    onTime = time;
+    return onTime;
+}
+
+boolean TimedDevice::updateStatus()
+{
+    unsigned long time;
+
+    time = increaseCurrentTime();
+    if (time >= onTime && enabled == true) {
+        rele->disable();
+        enabled = false;
+        resetCurrentTime();
+    } else if (time >= offTime && enabled == false) {
+        rele->enable();
+        enabled = true;
+        resetCurrentTime();
+    }
+
+    return enabled;
+}
+
+unsigned long TimedDevice::increaseCurrentTime()
+{
+    currentTime++;
+    return currentTime;
+}
+
+void TimedDevice::resetCurrentTime()
+{
+    currentTime = 0;
+}
+
 
 #endif
